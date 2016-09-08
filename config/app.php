1e89f4e50c8d7cb3137ffb2a9797a4926891ceef
drew beaman <ext-dbeaman@users.noreply.github.com>
2016-09-08 11:49:30 -0400

    debugbar

--- config/app.php
+++ config/app.php
@@ -155,6 +155,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Laracasts\Utilities\JavaScript\JavaScriptServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
 
 
@@ -215,6 +216,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
