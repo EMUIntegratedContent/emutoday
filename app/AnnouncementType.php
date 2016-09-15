b8710ad8a5e5e2b3813b9f8ecd2ccdcf8672cda1
drew beaman <ext-dbeaman@users.noreply.github.com>
2016-09-14 23:15:46 -0400

    adding announcement types

---
+++ app/AnnouncementType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Emutoday;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AnnouncementType extends Model
+{
+    //
+}
--- database/migrations/2016_09_15_031249_create_announcement_types_table.php
+++ database/migrations/2016_09_15_031249_create_announcement_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddSubmitterColForNonAdminUsers extends Migration
+class CreateAnnouncementTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,8 +12,11 @@ class AddSubmitterColForNonAdminUsers extends Migration
      */
     public function up()
     {
-        Schema::table('announcements', function (Blueprint $table) {
-            $table->string('submitter')->after('user_id');
+        Schema::create('announcement_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('shortname');
+            $table->timestamps();
         });
     }
 
@@ -24,9 +27,6 @@ class AddSubmitterColForNonAdminUsers extends Migration
      */
     public function down()
     {
-        Schema::table('announcements', function (Blueprint $table) {
-            $table->dropColumn('submitter');
-
-        });
+        Schema::drop('announcement_types');
     }
 }
