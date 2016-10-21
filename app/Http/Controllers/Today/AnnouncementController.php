35abb846054ac273d948b6db5c27cb4a61ab18b7
Brian Koscielniak <bkoscielniak31@gmail.com>
2016-10-21 16:17:36 -0400

    squashed announcements buggo, removed cruft

--- app/Http/Controllers/Today/AnnouncementController.php
+++ app/Http/Controllers/Today/AnnouncementController.php
@@ -68,39 +68,4 @@ class AnnouncementController extends Controller
       // return view('public.announcement.edit', compact('announcement'));
     }
 
-    public function store(Announcement $announcement, Request $request)
-    { // Method to validate and store announcements submission
-      $this->validate($request, [ // validate takes rules to test against, see laravel documentation for rules
-        // errors array is global. if validation fails, errors array will be populated
-        'title' => 'required|min:10|max:50',
-        'announcement' => 'required|min:20|max:255',
-        'link_txt' => 'required',
-        'link' => 'required',
-        'email_link_txt' => 'required',
-        'email_link' => 'required|email',
-        'start_date' => 'required|date',
-        'end_date' => 'required|date',
-      ]);
-
-      $write = $announcement->create([ // Is there a less verbose method to drop values in db table?
-        'is_approved' => '0',
-        'is_promoted' => '0',
-        'title' => $request->title,
-        'announcement' => $request->announcement,
-        'link_txt' => $request->link_txt,
-        'link' => $request->link,
-        'email_link_txt' => $request->email_link_txt,
-        'email_link' => $request->email_link,
-        'start_date' => $request->start_date,
-        'end_date' => $request->end_date,
-      ]);
-
-      if($write){
-        // Did the database write happen successfully? Return success
-        $success = true; return view('public.announcement.form', compact('success'));
-      } else {
-        // Laravel will display a SQL EXCEPTION error page. So this will naver return.
-        return 'Write to database failed.';
-      }
-    }
   }
