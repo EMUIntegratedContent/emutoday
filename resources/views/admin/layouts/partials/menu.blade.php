69fb423c60015408e3b5c224ad4e3c6b38b94d08
Brian Koscielniak <bkoscielniak31@gmail.com>
2016-11-03 10:39:41 -0400

    lbc user

--- resources/views/admin/layouts/partials/menu.blade.php
+++ resources/views/admin/layouts/partials/menu.blade.php
@@ -1,137 +1,139 @@
 
 @can('admin', $currentUser)
 <li class="treeview {{ set_active('admin/announcement*') }}">
-    <a href="#"><i class="fa fa-bullhorn"></i> <span>Announcements</span> <!-- <i class="fa fa-angle-left pull-right"></i> --></a>
-    <ul class="treeview-menu">
-        <li><a href="#"><i class="fa fa-circle-o"></i>General <span class="pull-right-container">
-          <i class="fa fa-angle-left pull-right"></i>
-        </span></a>
-        <ul class="treeview-menu {{ set_active('admin/announcement/*/general') }}">
-            <li class="{{ set_active('admin/announcement/*/general') }}"><a href="/admin/announcement/queue/general"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
-            {{-- <li class="{{ set_active('admin/announcement*') }}"><a href="/admin/announcement"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
-            <li class="{{ set_active('admin/announcement/*/general') }}"><a href="/admin/announcement/form/general"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
-        </ul>
-        </li>
-        <li><a href="#"><i class="fa fa-circle-o"></i>HR <span class="pull-right-container">
-          <i class="fa fa-angle-left pull-right"></i>
-        </span></a>
-        <ul class="treeview-menu {{ set_active('admin/announcement/*/hr') }}">
-            <li class="{{ set_active('admin/announcement/*/hr') }}"><a href="/admin/announcement/queue/hr"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
-            {{-- <li class="{{ set_active('admin/announcement*') }}"><a href="/admin/announcement"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
-            <li class="{{ set_active('admin/announcement/*/hr') }}"><a href="/admin/announcement/form/hr"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
-        </ul>
-        </li>
+  <a href="#"><i class="fa fa-bullhorn"></i> <span>Announcements</span> <!-- <i class="fa fa-angle-left pull-right"></i> --></a>
+  <ul class="treeview-menu">
+    <li><a href="#"><i class="fa fa-circle-o"></i>General <span class="pull-right-container">
+      <i class="fa fa-angle-left pull-right"></i>
+    </span></a>
+    <ul class="treeview-menu {{ set_active('admin/announcement/*/general') }}">
+      <li class="{{ set_active('admin/announcement/*/general') }}"><a href="/admin/announcement/queue/general"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
+      {{-- <li class="{{ set_active('admin/announcement*') }}"><a href="/admin/announcement"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
+      <li class="{{ set_active('admin/announcement/*/general') }}"><a href="/admin/announcement/form/general"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
     </ul>
+  </li>
+  <li><a href="#"><i class="fa fa-circle-o"></i>HR <span class="pull-right-container">
+    <i class="fa fa-angle-left pull-right"></i>
+  </span></a>
+  <ul class="treeview-menu {{ set_active('admin/announcement/*/hr') }}">
+    <li class="{{ set_active('admin/announcement/*/hr') }}"><a href="/admin/announcement/queue/hr"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
+    {{-- <li class="{{ set_active('admin/announcement*') }}"><a href="/admin/announcement"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
+    <li class="{{ set_active('admin/announcement/*/hr') }}"><a href="/admin/announcement/form/hr"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
+  </ul>
+</li>
+</ul>
 </li>
 <li class="treeview {{ set_active('admin/event*') }}">
-    <a href="#"><i class="fa fa-calendar"></i> <span>Events</span> <!-- <i class="fa fa-angle-left pull-right"></i>--></a>
-    <ul class="treeview-menu">
-        <li class="{{ set_active('admin/event*') }}"><a href="/admin/event/queue"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
-        <li class="{{ set_active('admin/event*') }}"><a href="/admin/event/lbcqueue"><i class="fa fa-university"></i> <span>LBC Approve</span></a></li>
-        {{-- <li class="{{ set_active('admin/event*') }}"><a href="/admin/event"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
-        <li class="{{ set_active('admin/event*') }}"><a href="/admin/event/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
-    </ul>
+  <a href="#"><i class="fa fa-calendar"></i> <span>Events</span> <!-- <i class="fa fa-angle-left pull-right"></i>--></a>
+  <ul class="treeview-menu">
+    <li class="{{ set_active('admin/event*') }}"><a href="/admin/event/queue"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
+    @can('lbc_approve', $currentUser)
+    <li class="{{ set_active('admin/event*') }}"><a href="/admin/event/lbcqueue"><i class="fa fa-university"></i> <span>LBC Approve</span></a></li>
+    @endcan
+    {{-- <li class="{{ set_active('admin/event*') }}"><a href="/admin/event"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
+    <li class="{{ set_active('admin/event*') }}"><a href="/admin/event/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
+  </ul>
 </li>
 @endcan
 @can('story_create', $currentUser)
 <li class="treeview {{ set_active('admin/story*') }}">
-    <a href="#"><i class="fa fa-file-text-o"></i> <span>Stories</span> <!-- <i class="fa fa-angle-left pull-right"> --></i></a>
-    <ul class="treeview-menu">
-        <li><a href="#"><i class="fa fa-circle-o"></i>News <span class="pull-right-container">
-          <i class="fa fa-angle-left pull-right"></i>
-        </span></a>
-        <ul class="treeview-menu {{ set_active('admin/story/news*') }}">
-                @can('story_promote', $currentUser)
-            <li class="{{ set_active('admin/story/news*') }}"><a href="/admin/story/news/queuenews"><i class="fa fa-rocket"></i> <span>News Queue</span></a></li>
-    @endcan
-            {{-- <li class="{{ set_active('admin/announcement*') }}"><a href="/admin/announcement"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
-            <li class="{{ set_active('admin/story*') }}"><a href="/admin/queuenews/story/news/form"><i class="fa fa-plus-square"></i><span>New News Story</span></a></li>
-        </ul>
-        </li>
-        <li><a href="#"><i class="fa fa-circle-o"></i>All Stories <span class="pull-right-container">
-          <i class="fa fa-angle-left pull-right"></i>
-        </span></a>
-        <ul class="treeview-menu {{ set_active('admin/story/all') }}">
-            @can('story_approve', $currentUser)
-            <li class="{{ set_active('admin/story*') }}"><a href="/admin/story/all/queueall"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
-    @endcan
-            {{-- <li class="{{ set_active('admin/announcement*') }}"><a href="/admin/announcement"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
-            <li class="{{ set_active('admin/story*') }}"><a href="/admin/queueall/story/story/form"><i class="fa fa-plus-square"></i><span>New Story</span></a></li>
-        </ul>
-        </li>
+  <a href="#"><i class="fa fa-file-text-o"></i> <span>Stories</span> <!-- <i class="fa fa-angle-left pull-right"> --></i></a>
+  <ul class="treeview-menu">
+    <li><a href="#"><i class="fa fa-circle-o"></i>News <span class="pull-right-container">
+      <i class="fa fa-angle-left pull-right"></i>
+    </span></a>
+    <ul class="treeview-menu {{ set_active('admin/story/news*') }}">
+      @can('story_promote', $currentUser)
+      <li class="{{ set_active('admin/story/news*') }}"><a href="/admin/story/news/queuenews"><i class="fa fa-rocket"></i> <span>News Queue</span></a></li>
+      @endcan
+      {{-- <li class="{{ set_active('admin/announcement*') }}"><a href="/admin/announcement"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
+      <li class="{{ set_active('admin/story*') }}"><a href="/admin/queuenews/story/news/form"><i class="fa fa-plus-square"></i><span>New News Story</span></a></li>
     </ul>
+  </li>
+  <li><a href="#"><i class="fa fa-circle-o"></i>All Stories <span class="pull-right-container">
+    <i class="fa fa-angle-left pull-right"></i>
+  </span></a>
+  <ul class="treeview-menu {{ set_active('admin/story/all') }}">
+    @can('story_approve', $currentUser)
+    <li class="{{ set_active('admin/story*') }}"><a href="/admin/story/all/queueall"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
+    @endcan
+    {{-- <li class="{{ set_active('admin/announcement*') }}"><a href="/admin/announcement"><i class="fa fa-list"></i> <span>List</span></a></li> --}}
+    <li class="{{ set_active('admin/story*') }}"><a href="/admin/queueall/story/story/form"><i class="fa fa-plus-square"></i><span>New Story</span></a></li>
+  </ul>
+</li>
+</ul>
 
 
 {{--
-    <ul class="treeview-menu">
-        @can('story_approve', $currentUser)
-        <li class="{{ set_active('admin/story*') }}"><a href="/admin/story/all/queueall"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
-        @endcan
-        <li class="{{ set_active('admin/story*') }}"><a href="/admin/queueall/story/story/form"><i class="fa fa-plus-square"></i><span>New Story</span></a></li>
+  <ul class="treeview-menu">
+    @can('story_approve', $currentUser)
+    <li class="{{ set_active('admin/story*') }}"><a href="/admin/story/all/queueall"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
+    @endcan
+    <li class="{{ set_active('admin/story*') }}"><a href="/admin/queueall/story/story/form"><i class="fa fa-plus-square"></i><span>New Story</span></a></li>
 
 
-        @can('story_promote', $currentUser)
+    @can('story_promote', $currentUser)
 
-        <li class="{{ set_active('admin/story/news*') }}"><a href="/admin/story/news/queuenews"><i class="fa fa-rocket"></i> <span>News Queue</span></a></li>
-        @endcan
-        <li class="{{ set_active('admin/story*') }}"><a href="/admin/queuenews/story/news/form"><i class="fa fa-plus-square"></i><span>New News Story</span></a></li>
+    <li class="{{ set_active('admin/story/news*') }}"><a href="/admin/story/news/queuenews"><i class="fa fa-rocket"></i> <span>News Queue</span></a></li>
+    @endcan
+    <li class="{{ set_active('admin/story*') }}"><a href="/admin/queuenews/story/news/form"><i class="fa fa-plus-square"></i><span>New News Story</span></a></li>
 
-    </ul> --}}
+  </ul> --}}
 </li>
 @endcan
 
 @can('admin', $currentUser)
 <li class="treeview {{ set_active('admin/page*') }}">
-    <a href="#"><i class="fa fa-newspaper-o"></i> <span>Hub</span> <!-- <i class="fa fa-angle-left pull-right"></i> --></a>
-    <ul class="treeview-menu">
-        <li class="{{ set_active('admin/page*') }}"><a href="/admin/page"><i class="fa fa-list"></i> <span>List</span></a></li>
-        <li class="{{ set_active('admin/page*') }}"><a href="/admin/page/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
-    </ul>
+  <a href="#"><i class="fa fa-newspaper-o"></i> <span>Hub</span> <!-- <i class="fa fa-angle-left pull-right"></i> --></a>
+  <ul class="treeview-menu">
+    <li class="{{ set_active('admin/page*') }}"><a href="/admin/page"><i class="fa fa-list"></i> <span>List</span></a></li>
+    <li class="{{ set_active('admin/page*') }}"><a href="/admin/page/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
+  </ul>
 </li>
 @endcan
 {{-- @can('super',$currentUser)
 <li class="treeview {{ set_active('admin/page*') }}">
-    <a href="#"><i class="fa fa-envelope-o"></i> <span>Email Blasts</span> <!-- <i class="fa fa-angle-left pull-right"> --></i></a>
-    <ul class="treeview-menu">
-        <li class="{{ set_active('admin/emailblast*') }}"><a href="#"><i class="fa fa-list"></i> <span>List</span></a></li>
-        <li class="{{ set_active('admin/emailblast*') }}"><a href="#"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
-    </ul>
+  <a href="#"><i class="fa fa-envelope-o"></i> <span>Email Blasts</span> <!-- <i class="fa fa-angle-left pull-right"> --></i></a>
+  <ul class="treeview-menu">
+    <li class="{{ set_active('admin/emailblast*') }}"><a href="#"><i class="fa fa-list"></i> <span>List</span></a></li>
+    <li class="{{ set_active('admin/emailblast*') }}"><a href="#"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
+  </ul>
 </li>
 @endcan --}}
 
 @can('admin', $currentUser)
 <li class="treeview {{ set_active('admin/magazine*') }}">
-    <a href="#"><i class="fa fa-book"></i> <span>Magazine</span> <!-- <i class="fa fa-angle-left pull-right"></i> --> </a>
-    <ul class="treeview-menu">
-        <li class="{{ set_active('admin/magazine*') }}"><a href="/admin/magazine"><i class="fa fa-list"></i> <span>List</span></a></li>
-        <li class="{{ set_active('admin/magazine*') }}"><a href="/admin/magazine/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
-        <li class="{{ set_active('admin/magazine*') }}"><a href="/admin/magazine/article/queuearticle"><i class="fa fa-rocket"></i><span>Articles</span></a></li>
-        <li class="{{ set_active('admin/magazine*') }}"><a href="/admin/queuearticle/magazine/article/form"><i class="fa fa-plus-square"></i><span>Add Article</span></a></li>
-    </ul>
+  <a href="#"><i class="fa fa-book"></i> <span>Magazine</span> <!-- <i class="fa fa-angle-left pull-right"></i> --> </a>
+  <ul class="treeview-menu">
+    <li class="{{ set_active('admin/magazine*') }}"><a href="/admin/magazine"><i class="fa fa-list"></i> <span>List</span></a></li>
+    <li class="{{ set_active('admin/magazine*') }}"><a href="/admin/magazine/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
+    <li class="{{ set_active('admin/magazine*') }}"><a href="/admin/magazine/article/queuearticle"><i class="fa fa-rocket"></i><span>Articles</span></a></li>
+    <li class="{{ set_active('admin/magazine*') }}"><a href="/admin/queuearticle/magazine/article/form"><i class="fa fa-plus-square"></i><span>Add Article</span></a></li>
+  </ul>
 </li>
 @endcan
 @can('super', $currentUser)
 <li class="treeview {{ set_active('admin/storyimages*') }}">
-    <a href="#"><i class="fa fa-picture-o"></i> <span>Images</span> <!-- <i class="fa fa-angle-left pull-right"></i> --> </a>
-    <ul class="treeview-menu">
-        <li class="{{ set_active('admin/storyimages*') }}"><a href="/admin/storyimages"><i class="fa fa-list"></i> <span>List</span></a></li>
-        <li class="{{ set_active('admin/storyimages*') }}"><a href="/admin/storyimages/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
-    </ul>
+  <a href="#"><i class="fa fa-picture-o"></i> <span>Images</span> <!-- <i class="fa fa-angle-left pull-right"></i> --> </a>
+  <ul class="treeview-menu">
+    <li class="{{ set_active('admin/storyimages*') }}"><a href="/admin/storyimages"><i class="fa fa-list"></i> <span>List</span></a></li>
+    <li class="{{ set_active('admin/storyimages*') }}"><a href="/admin/storyimages/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
+  </ul>
 </li>
 
 <hr/> <!-- //////////////////////////////// -->
 
 <li class="{{ set_active('admin/user*') }}"><a href="/admin/user"><i class="fa fa-users"></i> <span>Users</span></a></li>
 {{--
-<li class="treeview {{ set_active('admin/twitter*') }}"><a href="/admin/twitter"><i class="fa fa-twitter"></i> <span>Tweets</span></a></li>
+  <li class="treeview {{ set_active('admin/twitter*') }}"><a href="/admin/twitter"><i class="fa fa-twitter"></i> <span>Tweets</span></a></li>
 
-<li class="treeview {{ set_active('admin/bugz*') }}">
+  <li class="treeview {{ set_active('admin/bugz*') }}">
     <a href="#"><i class="fa fa-bug"></i> <span>Bugz</span> <!-- <i class="fa fa-angle-left pull-right"></i> --> </a>
     <ul class="treeview-menu">
-        <li class="{{ set_active('admin/bugz*') }}"><a href="/admin/bugz/app"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
-        <li class="{{ set_active('admin/bugz*') }}"><a href="/admin/bugz"><i class="fa fa-list"></i> <span>List</span></a></li>
-        <li class="{{ set_active('admin/bugz*') }}"><a href="/admin/bugz/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
+      <li class="{{ set_active('admin/bugz*') }}"><a href="/admin/bugz/app"><i class="fa fa-rocket"></i> <span>Queue</span></a></li>
+      <li class="{{ set_active('admin/bugz*') }}"><a href="/admin/bugz"><i class="fa fa-list"></i> <span>List</span></a></li>
+      <li class="{{ set_active('admin/bugz*') }}"><a href="/admin/bugz/form"><i class="fa fa-plus-square"></i> <span>Create</span></a></li>
     </ul>
-</li>
---}}
-@endcan
+  </li>
+  --}}
+  @endcan
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -2,120 +2,112 @@
 
 @section('title', 'Users')
 
-    @section('content')
-      <div class="row">
-                        <div class="col-xs-12">
-                            <div class="box">
-                                <div class="box-header with-border">
-                                    <h3 class="box-title">User Table</h3>
-                                    @can('super', $currentUser)
-                                    @include('admin.components.boxtools', ['rte' => 'user', 'path' => 'admin/user'])
-                                @endcan
-                                </div>
-                                <!-- /.box-header -->
-                                <div class="box-body table-responsive no-padding">
-                                    <table class="table table-hover">
-                                        <tr>
-                                            <th>Id</th>
-                                            <th>Email</th>
-                            <th>Last Name</th>
-                                            <th>First Name</th>
-                                            <th>Phone</th>
-                                            <th>Roles</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                                        </tr>
-                                        <tr>
-                                            @foreach($users as $user)
-                                    <tr>
-                                                        <td>
-                                                                <a href="#">{{ $user->id }}</a>
-                                                        </td>
-                                                        <td>
-                                                                <a href="/admin/user/{{ $user->id}}">{{ $user->email }}</a>
-                                                        </td>
-                                                          <td>{{ $user->last_name }}</td>
-                                                            <td>{{ $user->first_name }}</td>
+@section('content')
+<div class="row">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header with-border">
+        <h3 class="box-title">User Table</h3>
+        @can('super', $currentUser)
+        @include('admin.components.boxtools', ['rte' => 'user', 'path' => 'admin/user'])
+        @endcan
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+          <tr>
+            <th>Id</th>
+            <th>Email</th>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>Phone</th>
+            <th>Roles</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+          <tr>
+            @foreach($users as $user)
+            <tr>
+              <td>
+                <a href="#">{{ $user->id }}</a>
+              </td>
+              <td>
+                <a href="/admin/user/{{ $user->id}}">{{ $user->email }}</a>
+              </td>
+              <td>{{ $user->last_name }}</td>
+              <td>{{ $user->first_name }}</td>
+              <td>{{ $user->phone }}</td>
+              <td>{{ $user->roles->pluck('name') }}</td>
+              <td>
+                <a href="/admin/user/{{ $user->id}}/edit">
+                  <i class="fa fa-pencil"></i>
+                </a>
+              </td>
+              <td>
+                <a href="{{ route('admin_user_confirm', $user->id) }}">
+                  <i class="fa fa-trash"></i>
+                </a>
+              </td>
+            </tr>
+            @endforeach
+          </tr>
+        </table>
+      </div>
+      <!-- /.box-body -->
+      <div class="box-footer text-right">
+        {!! $users->render() !!}
+      </div> <!-- /.box-footer -->
+    </div>
+    <!-- /.box -->
+  </div>
+</div>
 
-                                        <td>{{ $user->phone }}</td>
-                                                            <td>{{ $user->roles->pluck('name') }}</td>
-                                        <td>
-                                            <a href="/admin/user/{{ $user->id}}/edit">
-                                                                        <i class="fa fa-pencil"></i>
-
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a href="{{ route('admin_user_confirm', $user->id) }}">
-                                                        <i class="fa fa-trash"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                        </tr>
-
-                                    </table>
-                                </div>
-                                <!-- /.box-body -->
-                                <div class="box-footer text-right">
-                                            {!! $users->render() !!}
-                                </div> <!-- /.box-footer -->
-                            </div>
-                            <!-- /.box -->
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="box">
-                                <div class="box-header with-border">
-                                    <h3 class="box-title">User Roles</h3>
-                                </div><!-- /.box-header -->
-                                <div class="box-body table-responsive no-padding">
-                                    <table class="table table-hover">
-                                        <tr>
-                                            <th>Id</th>
-                                            <th>Role</th>
-                                            <th>Label</th>
-
-                                        </tr>
-                                            @foreach($roles as $role)
-                                                    <tr>
-                                                        <td>{{$role->id}}</td>
-                                                        <td>{{ $role->name }}</td>
-                                                        <td>{{ $role->label }}</td>
-                                                    </tr>
-                                            @endforeach
-                        </table>
-                            </div><!-- /.box-body -->
-                        </div><!-- /.box -->
-                        </div><!-- /.col-md-6 -->
-                        <div class="col-md-6">
-                            <div class="box">
-                                <div class="box-header with-border">
-                                    <h3 class="box-title">User Permissions</h3>
-                                </div><!-- /.box-header -->
-                                <div class="box-body table-responsive no-padding">
-                                    <table class="table table-hover">
-                                        <tr>
-                                            <th>Id</th>
-                                            <th>Name</th>
-                                            <th>Label</th>
-
-                                        </tr>
-                                            @foreach($permissions as $permission)
-                                                    <tr>
-                                                        <td>{{$permission->id}}</td>
-                                                        <td>{{ $permission->name }}</td>
-                                                        <td>{{ $permission->label }}</td>
-                                                    </tr>
-                                            @endforeach
-                        </table>
-                            </div><!-- /.box-body -->
-                        </div><!-- /.box -->
-                        </div><!-- /.col-md-6 -->
-                    </div><!-- /.row -->
-
-
-
-    @endsection
+<div class="row">
+  <div class="col-md-6">
+    <div class="box">
+      <div class="box-header with-border">
+        <h3 class="box-title">User Roles</h3>
+      </div><!-- /.box-header -->
+      <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+          <tr>
+            <th>Id</th>
+            <th>Role</th>
+            <th>Label</th>
+          </tr>
+          @foreach($roles as $role)
+          <tr>
+            <td>{{$role->id}}</td>
+            <td>{{ $role->name }}</td>
+            <td>{{ $role->label }}</td>
+          </tr>
+          @endforeach
+        </table>
+      </div><!-- /.box-body -->
+    </div><!-- /.box -->
+  </div><!-- /.col-md-6 -->
+  <div class="col-md-6">
+    <div class="box">
+      <div class="box-header with-border">
+        <h3 class="box-title">User Permissions</h3>
+      </div><!-- /.box-header -->
+      <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Label</th>
+          </tr>
+          @foreach($permissions as $permission)
+          <tr>
+            <td>{{$permission->id}}</td>
+            <td>{{ $permission->name }}</td>
+            <td>{{ $permission->label }}</td>
+          </tr>
+          @endforeach
+        </table>
+      </div><!-- /.box-body -->
+    </div><!-- /.box -->
+  </div><!-- /.col-md-6 -->
+</div><!-- /.row -->
+@endsection
