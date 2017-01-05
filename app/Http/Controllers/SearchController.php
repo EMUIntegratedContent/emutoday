5b21467a7c247d4f7a2187d0adebfbb7fe13b55a
Brian Koscielniak <bkoscielniak31@gmail.com>
2017-01-05 13:36:45 -0500

    Search "approved" only

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -61,7 +61,9 @@ class SearchController extends Controller
                 'content' => 35,
                 'teaser' => 20,
                 'subtitle' => 10,
-            ], false)->select('title','subtitle','story_type','teaser','id')->get();
+            ], false)->select('title','subtitle','story_type','teaser','id')->where('is_approved', 1)->get();
+
+                // ['is_approved', 1],
         } else {
             $searchStoryResults = array();
         }
@@ -70,7 +72,7 @@ class SearchController extends Controller
         if(!$filter || $filter == 'events' || $filter == 'all'){
             $searchEventResults = Event::search($searchTermWild, [
                 'title' => 10,
-            ], false)->get();
+            ], false)->where('is_approved', 1)->get();
         } else {
             $searchEventResults = array();
         }
@@ -80,7 +82,7 @@ class SearchController extends Controller
             $searchAnnouncementResults = Announcement::search($searchTermWild, [
                 'title' => 50,
                 'announcement' => 35
-            ], false)->select('title','announcement','submitter','id')->get();
+            ], false)->where('is_approved', 1)->select('title','announcement','submitter','id')->get();
         } else {
             $searchAnnouncementResults = array();
         }
@@ -92,7 +94,7 @@ class SearchController extends Controller
                 'content' => 35,
                 'teaser' => 20,
                 'subtitle' => 10,
-            ], false)->select('title','subtitle','story_type','teaser','id')->get();
+            ], false)->where('is_approved', 1)->select('title','subtitle','story_type','teaser','id')->get();
 
             $searchMagazineResults = array();
 
