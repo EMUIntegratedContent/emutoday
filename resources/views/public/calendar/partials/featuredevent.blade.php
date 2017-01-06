6df6a0c9ac5335fc58bd3d246d86a04a3f558ff4
Brian Koscielniak <bkoscielniak31@gmail.com>
2017-01-06 16:02:38 -0500

    featured event calendar all day event display fix

--- resources/views/public/calendar/partials/featuredevent.blade.php
+++ resources/views/public/calendar/partials/featuredevent.blade.php
@@ -4,8 +4,11 @@
   <div class="calendar-content">
     <div class="calendar-text-content" data-equalizer-watch>
       <h6><a href="/calendar/{{$fevent->present()->eventStartDateYear}}/{{$fevent->present()->eventStartDateMonthUnit}}/{{$fevent->present()->eventStartDateDay}}/{{$fevent->id}}">{{$fevent->title}}</a></h6>
-      <p>{{$fevent->present()->displayTimeRange }}</p>
-      <p>{{$fevent->location}}</p>
+      @if ($fevent->all_day)
+        <p>All Day</p>
+      @else
+        <p>{{$fevent->present()->displayTimeRange }}</p>
+      @endif
     </div>
   </div>
 </div>
