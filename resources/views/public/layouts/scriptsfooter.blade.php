7e41aaf845eb7bc2e7b0838ad3c2bffd91db531d
emulibrary <emulibrary@gmail.com>
2017-02-07 15:24:53 -0500

    trying to fix IE accordion bug

--- resources/views/public/layouts/scriptsfooter.blade.php
+++ resources/views/public/layouts/scriptsfooter.blade.php
@@ -1,6 +1,8 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript" src="/js/public-scripts.js"></script>
-<!--<script type="text/javascript" src="/js/foundation.min.js"></script>-->
+<!--[if IE]>
+    <script type="text/javascript" src="/js/foundation.min.js"></script>
+<![endif]-->
 <script type="text/javascript" src="/js/vue-search-form.js"></script>
 <script type="text/javascript" src="/js/vue-search-form-offcanvas.js"></script>
 <script type="text/javascript" src="/js/emergency.js"></script>
