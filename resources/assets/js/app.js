df398efe97401bf56811473e43879a0c0a0a4be4
emulibrary <emulibrary@gmail.com>
2017-02-07 15:21:41 -0500

    trying to fix IE accordion bug

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,28 @@
-//$(document).foundation();
+$(document).foundation();
+$(document).ready(function() {
+  $('.popup-youtube').magnificPopup({
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  });
+
+  /**
+   * These blocks ensure image captions do not stretch image dimensions on news stories
+   */
+
+  $.each($('figure img'), function() {
+      var imgWidth = $(this).width();
+      var figureWidth = imgWidth + 40; //38px ~ 1.11rem x 2(sides)
+      $(this).closest('figure').css({'width': figureWidth, 'overflow-wrap': 'break-word'});
+
+      console.log(imgWidth);
+  });
+  $.each($('.visbox img'), function() {
+      var imgWidth = $(this).width();
+      $(this).closest('div.visbox').css({'width': imgWidth, 'overflow-wrap': 'break-word'});
+
+      console.log(imgWidth);
+  });
+});
--- resources/views/public/layouts/scriptsfooter.blade.php
+++ resources/views/public/layouts/scriptsfooter.blade.php
@@ -5,34 +5,5 @@
 <script type="text/javascript" src="/js/vue-search-form-offcanvas.js"></script>
 <script type="text/javascript" src="/js/emergency.js"></script>
 <script type="text/javascript" src="/js/jquery.magnific-popup.js"></script>
-<script>
-    $(document).foundation();
-    $(document).ready(function() {
-      $('.popup-youtube').magnificPopup({
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false,
-      });
-
-      /**
-       * These blocks ensure image captions do not stretch image dimensions on news stories
-       */
-
-      $.each($('figure img'), function() {
-          var imgWidth = $(this).width();
-          var figureWidth = imgWidth + 40; //38px ~ 1.11rem x 2(sides)
-          $(this).closest('figure').css({'width': figureWidth, 'overflow-wrap': 'break-word'});
-
-          console.log(imgWidth);
-      });
-      $.each($('.visbox img'), function() {
-          var imgWidth = $(this).width();
-          $(this).closest('div.visbox').css({'width': imgWidth, 'overflow-wrap': 'break-word'});
-
-          console.log(imgWidth);
-      });
-    });
-</script>
+<script type="text/javascript" src="/js/app.js"></script>
 @yield('scriptsfooter')
