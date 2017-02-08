82bffce6fa6326435f811b9c040726cd627f6fd2
Brian Koscielniak <bkoscielniak31@gmail.com>
2017-02-08 11:44:23 -0500

    Captions ability on Featured events

--- resources/views/public/calendar/partials/featuredevent.blade.php
+++ resources/views/public/calendar/partials/featuredevent.blade.php
@@ -4,7 +4,7 @@
   <div class="calendar-content">
     <div class="calendar-text-content" data-equalizer-watch>
       <h6><a href="/calendar/{{$fevent->present()->eventStartDateYear}}/{{$fevent->present()->eventStartDateMonthUnit}}/{{$fevent->present()->eventStartDateDay}}/{{$fevent->id}}">{{$fevent->title}}</a></h6>
-      @if (preg_match("/\s+/", $fevent->mediaFile->caption) == false && $fevent->mediaFile->caption != '' && $fevent->mediaFile->caption != NULL && $fevent->mediaFile->caption != 'null')
+      @if (preg_match("/^\s+$/", $fevent->mediaFile->caption) == false && $fevent->mediaFile->caption != '' && $fevent->mediaFile->caption != NULL && $fevent->mediaFile->caption != 'null')
         <p>{{$fevent->mediaFile->caption}}</p>
       @else
         @if ($fevent->all_day)
