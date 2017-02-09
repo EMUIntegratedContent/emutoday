1b520e3ffea241d2046f42ce2b45a9eb86343ed7
emulibrary <emulibrary@gmail.com>
2017-02-09 09:20:50 -0500

    ie upgrade alert bar unstyled

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 
   // Internet Explorer 6-11 (http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser)
   var isIE = /*@cc_on!@*/false || !!document.documentMode;
-  if(isIE){
+  if(!isIE){
       $('#outdated-browser-container').html('<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/?locale=en">upgrade your browser</a> to improve your experience.</p>');
   }
 });
