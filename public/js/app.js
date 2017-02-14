f5057ad3d1d39142322e066951608cafdd46d582
Brian Koscielniak <bkoscielniak31@gmail.com>
2017-02-14 10:19:41 -0500

    styled outdated browser alert

--- public/js/app.js
+++ public/js/app.js
@@ -30,10 +30,10 @@ $(document).ready(function () {
     });
 
     // Internet Explorer 6-11 (http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser)
-    // var isIE = /*@cc_on!@*/false || !!document.documentMode;
-    // if (isIE) {
-    //     $('#outdated-browser-container').html('<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/?locale=en">upgrade your browser</a> to improve your experience.</p>');
-    // }
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    if (!isIE) {
+        $('#outdated-browser-container').html('<p data-alert class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/?locale=en">upgrade your browser</a> to improve your experience.</p>');
+    }
 });
 
 },{}]},{},[1]);
