e012b844f96f248e3592914ec2be1585d1276f24
Chris Puzzuoli <cpuzzuol@emich.edu>
2017-10-04 17:51:47 -0400

    iCal route for events feed...plus Brian's changes

--- app/Http/Controllers/Today/RSSFeedController.php
+++ app/Http/Controllers/Today/RSSFeedController.php
@@ -42,7 +42,7 @@ class RSSFeedController extends Controller {
   }
 
   public function getEventsICal(){
-    $events = Event::where([['is_approved', 1], ['start_date', '>=', date('Y-m-d H:i:s')]])->orderBy('start_date', 'asc')->paginate(5);
+    $events = Event::where([['is_approved', 1], ['start_date', '>=', date('Y-m-d H:i:s')]])->orderBy('start_date', 'asc')->paginate(30);
     // the iCal date format. Note the Z on the end indicates a UTC timestamp.
     define('DATE_ICAL', 'Ymd\THis\Z');
 
