fb23a2af83b9eac3d562fd13101c03d8d059631e
Chris Puzzuoli <cpuzzuol@emich.edu>
2017-10-24 15:51:42 -0400

    trying out mailgun webhooks/API

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         'api/*',
         'externalapi/*',
-        'oauth/*'
+        'oauth/*',
+        'mailgun/*'
     ];
 }
