187e9d979cabad1b16161b3759a6ef98a14f28ca
Chris Puzzuoli <cpuzzuol@emich.edu>
2017-10-24 15:47:00 -0400

    trying out mailgun webhooks/API

--- app/Http/Controllers/Api/MailgunApiController.php
+++ app/Http/Controllers/Api/MailgunApiController.php
@@ -3,19 +3,12 @@
 namespace Emutoday\Http\Controllers\Api;
 
 use Log;
-use Emutoday\Announcement;
-use Emutoday\Event;
-use Emutoday\Story;
-use Emutoday\MiniCalendar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input as Input;
 use Carbon\Carbon;
-use League\Fractal\Manager;
-use League\Fractal;
 use League\Fractal\Serializer\ArraySerializer;
 use League\Fractal\Serializer\DataArraySerializer;
 
-use Emutoday\Today\Transformers\FractalAnnouncementTransformerModel;
 class MailgunApiController extends ApiController
 {
   function __construct()
@@ -24,8 +17,8 @@ class MailgunApiController extends ApiController
   }
 
   public function postClick(Request $request){
-    Log::info('MAILGUN WUZ HERE');
-    
+    //Log::info('MAILGUN WUZ HERE');
+
     return $this->setStatusCode(200)
     ->respondUpdatedWithData('Hello API world!',[]);
   }
