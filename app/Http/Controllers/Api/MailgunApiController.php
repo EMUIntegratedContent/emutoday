661e9000a5410435ec323208c7b4ebddf1f02f2c
Chris Puzzuoli <cpuzzuol@emich.edu>
2017-10-24 16:10:15 -0400

    trying out mailgun webhooks/API

--- app/Http/Controllers/Api/MailgunApiController.php
+++ app/Http/Controllers/Api/MailgunApiController.php
@@ -19,17 +19,24 @@ class MailgunApiController extends ApiController
   public function postClick(Request $request){
     //Log::info('MAILGUN WUZ HERE');
 
+/*
     if($this->verify()){
       return $this->setStatusCode(200)
       ->respond('Hello API world!');
     }
-
+*/
     return $this->setStatusCode(401)
-    ->respond('Unauthenticated usage of this route!');
+    ->respondUpdatedWithData('here is some data', $request->all());
 
   }
 
-  protected function verify($api_key, $token, $timestamp, $signature){
-    return false;
+  private function verify($apiKey, $token, $timestamp, $signature){
+    //check if the timestamp is fresh
+    if (abs(time() - $timestamp) > 15) {
+        return false;
+    }
+
+    //returns true if signature is valid
+    return hash_hmac('sha256', $timestamp.$token, $apiKey) === $signature;
   }
 }
