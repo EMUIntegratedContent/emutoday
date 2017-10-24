56eb881e5a866a9f03064be203f284140b688720
Chris Puzzuoli <cpuzzuol@emich.edu>
2017-10-24 18:08:56 -0400

    update mailgun webhooks controller

--- app/Http/Controllers/Api/MailgunApiController.php
+++ app/Http/Controllers/Api/MailgunApiController.php
@@ -3,6 +3,7 @@
 namespace Emutoday\Http\Controllers\Api;
 
 use Log;
+use Emutoday\Email;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input as Input;
 use Carbon\Carbon;
@@ -22,15 +23,34 @@ class MailgunApiController extends ApiController
 
     // Secure the Webhook endpoint (Note: API key comes from .env file)
     if($this->verify(env('MAILGUN_SECRET'), $mailgun_post_data['token'], $mailgun_post_data['timestamp'], $mailgun_post_data['signature'])){
+      if(!$this->incrementOpen()){
+        return $this->setStatusCode(400)
+        ->respond('Email not found.');
+      }
       return $this->setStatusCode(200)
-      ->respond('Hello API world!');
+      ->respond('Email open counted.');
     }
-
     return $this->setStatusCode(401)
     ->respond('Unauthenticated usage of this route!');
-
   }
 
+  /**
+   * Increase the number of times this email has been opened by incrementing the mailgun_opens field
+   * @param int $email_id   The EMU Today database ID of the email
+   *
+   * @return boolean
+   */
+  private function incrementOpen($email_id){
+    $email = Email::find($email_id);
+
+    if($email){
+      $email->mailgun_opens += 1;
+      if($email->save()){
+        return true;
+      }
+    }
+    return false;
+  }
   /**
    * To ensure the authenticity of event requests, Mailgun signs them and posts the signature along with other webhook parameters:
    * DOCS: https://documentation.mailgun.com/en/latest/user_manual.html#webhooks
