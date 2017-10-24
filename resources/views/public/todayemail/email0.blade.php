c08ed619840a1336f4cee582d7ef877ccf3a7b92
Chris Puzzuoli <cpuzzuol@emich.edu>
2017-10-24 14:35:40 -0400

    email template edits

--- resources/views/public/todayemail/email0.blade.php
+++ resources/views/public/todayemail/email0.blade.php
@@ -1,4 +1,4 @@
-<div style="border:1px solid #000000; padding:5px; margin: 0 auto; width:600px">
+<div style="border:1px solid #000000; padding:5px; margin: 0 auto; width:610px">
 	<table border="0" cellpadding="5" cellspacing="0" height="100%" width="100%">
 			<tr>
 					<td align="center" valign="top">
