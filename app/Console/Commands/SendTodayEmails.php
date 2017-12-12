4dd4b96af30fa42062883c63b64055f1cd3d17de
Chris Puzzuoli <cpuzzuol@emich.edu>
2017-12-12 13:07:44 -0500

    changed email FROM title

--- app/Console/Commands/SendTodayEmails.php
+++ app/Console/Commands/SendTodayEmails.php
@@ -70,7 +70,7 @@ class SendTodayEmails extends Command
         // Send one email to each recipient/mailing list
         foreach($email->recipients as $recipient){
           Mail::send('public.todayemail.email', ['email' => $email, 'mainStories' => $mainStories, 'mainStoryImages' => $mainStoryImages], function ($message) use ($email, $recipient){
-              $message->from(env('MAIL_USERNAME', 'noreply@today.emich.edu'), 'EMU Today');
+              $message->from(env('MAIL_USERNAME', 'noreply@today.emich.edu'), 'The Week at EMU');
               $message->subject($email->title);
               $message->to($recipient->email_address);
 
