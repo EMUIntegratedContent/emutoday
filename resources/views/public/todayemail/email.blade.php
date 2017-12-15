87aa8a87bc24c7c073bfe7f93fe4acf4f8b31ba2
Suzanne Szopo <bszopo@emich.edu>
2017-12-15 16:04:31 -0500

    email update

--- resources/views/public/todayemail/email.blade.php
+++ resources/views/public/todayemail/email.blade.php
@@ -8,7 +8,7 @@
         /*background-color: #f3f2ee;*/
         /*background-color: #d6d2c4;*/
         /*background-color: #e6e6e6;*/
-        background-color: #cccccc;
+        background-color: #d6d6d6;
     }
     table {
         border-collapse: collapse;
