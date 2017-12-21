4aa3130d18cbb7904528bdc191b0ed0ffa2c5ccd
Chris Puzzuoli <cpuzzuol@emich.edu>
2017-12-21 14:23:41 -0500

    tweak to news filter on hub page

--- resources/views/public/hub.blade.php
+++ resources/views/public/hub.blade.php
@@ -115,6 +115,7 @@
           </div>
           <div class="tabs-panel newshub-tab-front" id="newshub-headlines-front">
             <ul>
+              {{ dd($currentStorysBasic) }}
               @foreach ($currentStorysBasic as $basicstory)
               <li>
                 @if($basicstory->story_type == 'advisory')
