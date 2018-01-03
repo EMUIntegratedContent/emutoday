5694c616b3ffaea9969df33e74c266ce17e3e0a7
Chris Puzzuoli <cpuzzuol@emich.edu>
2018-01-03 14:21:34 -0500

    minor changes to media resources page

--- resources/views/public/forthemedia.blade.php
+++ resources/views/public/forthemedia.blade.php
@@ -6,7 +6,7 @@
       <div class="row">
         <div class="large-12 medium-12 small-12 column">
           <div id="title-grouping" class="row">
-            <div class="large-6 medium-12 columns"><h3 class="news-caps">Media Resources</h3></div>
+            <div class="large-6 medium-12 columns"><h3 class="news-caps">For the Media</h3></div>
           </div>
           <div class="row">
               <div class="large-3 medium-3 small-12 columns">
@@ -17,25 +17,25 @@
                   </ul>
               </div>
               <div class="large-9 medium-9 small-12 columns ">
-                <h1>For the Media</h1>
+                <h1 style="padding-top:7px;">Media Resources</h1>
                 <p>The media relations team writes and distributes content about faculty research, student accomplishments and general EMU initiatives to local, state and national media outlets. We also help manage crisis communications for the university, respond to media inquiries, provide media training to faculty and administrators, and advise the colleges on communication strategies.</p>
                 <h2>Contact</h2>
                 <p>
                   Geoff Larcom, Executive Director, Media Relations<br />
                   734.487.4400<br />
                   cell: 734.417.9658<br />
-                  glarcom@emich.edu<br />
-                  Resources for Media
+                  glarcom@emich.edu
                 </p>
                 <h2>Details about Eastern Michigan University</h2>
                 <ul>
-                  <li>President James Smith</li>
-                  <li>Board of Regents</li>
-                  <li>Safety Initiatives</li>
-                  <li>University Facts</li>
-                  <li>Enrollment Statistics</li>
-                  <li>Freedom of Information Act (FOIA)</li>
-                  <li>Extreme Weather Policy</li>
+                  <li><a href="https://www.emich.edu/president/">President James Smith</a></li>
+                  <li><a href="https://www.emich.edu/regents/">Board of Regents</a></li>
+                  <li><a href="https://www.emich.edu/police/about/safety-initiatives.php">Safety Initiatives</a></li>
+                  <li><a href="https://www.emich.edu/facts/index.php">University Facts</a></li>
+                  <li><a href="https://irim.emich.edu/">Enrollment Statistics</a></li>
+                  <li><a href="https://www.emich.edu/legalaffairs/foia.php">Freedom of Information Act (FOIA)</a></li>
+                  <li><a href="https://www.emich.edu/legalaffairs/foia.php">Extreme Weather Policy</a></li>
+                </ul>
               </div>
           </div>
         </div>
