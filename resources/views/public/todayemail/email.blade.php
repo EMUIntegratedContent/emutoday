8e1e178691f1461f358424064da1518c5ea9d364
Suzanne Szopo <bszopo@emich.edu>
2018-01-09 10:43:37 -0500

    try to fix images

--- resources/views/public/todayemail/email.blade.php
+++ resources/views/public/todayemail/email.blade.php
@@ -177,12 +177,20 @@
      .card-container .card.right-card{
         margin-left: 0; width: 46%; margin-right: 0; float: right; position: relative; box-sizing: border-box; display: inline-block; max-width: 100%;background-color: orange;
     }
+    
+    
+    .card-container .card .imagebox{
+        margin-bottom: 0px; color: #ffffff; text-decoration: none; position: relative; box-sizing: border-box;
+    }
+    
+    .card-container .card .text-box{
+        padding-top: 5px; padding-bottom: 10px; float: left; position: relative; box-sizing: border-box;
+    }
 
 </style>
 
 
 <style>
-    /*Media Queries*/
 @media only screen and (min-device-width: 610px) {
   .card-container{
         display: none;  
@@ -264,9 +272,9 @@
                                         <!--<div class="card" style="margin-left: 0; width: 47%; margin-right: 6%;  float: left; position: relative; box-sizing: border-box; display: inline-block; max-width: 100%;">-->
                                             <div class="card">
 
-                                            <div class="imagebox" style="margin-bottom: 0px; color: #ffffff; text-decoration: none;"><img class="col-img" alt="{{ $mainStoryImages[1]->caption }}" src="{{ url('/') }}/imagecache/emailsub/{{$smallStoryImages[1]->filename}}" /></div>
+                                            <div class="imagebox"><img class="col-img" alt="{{ $mainStoryImages[1]->caption }}" src="{{ url('/') }}/imagecache/emailsub/{{$smallStoryImages[1]->filename}}" /></div>
 
-                                            <div class="text-box" style="padding-top: 5px; padding-bottom: 10px; float: left; position: relative; box-sizing: border-box;">
+                                            <div class="text-box">
                                                  <h3 class="mid"><a href="{{ url('/') . '/story/' . $mainStories[1]->story_type . '/' . $mainStories[1]->id }}">{{ $mainStoryImages[1]->title }} &#10137;</a></h3>
                                                                                     {{--<p>{!! str_limit($mainStoryImages[1]->teaser, $limit = 110, $end = '...') !!}</p>--}}
                                                                                     {!! truncateLimitWords($mainStoryImages[1]->teaser, 110) !!}
@@ -277,9 +285,9 @@
                                        <!-- <div class="card right-card" style="margin-left: 0; width: 47%;  float: right; position: relative; box-sizing: border-box; display: inline-block; max-width: 100%;">-->
                                             <div class="card right-card">
 
-                                            <div class="imagebox" style="margin-bottom: 0px; color: #ffffff; text-decoration: none;"><img  class="col-img" alt="{{ $mainStoryImages[2]->caption }}" src="{{ url('/') }}/imagecache/emailsub/{{$smallStoryImages[2]->filename}}" /></div>
+                                            <div class="imagebox"><img  class="col-img" alt="{{ $mainStoryImages[2]->caption }}" src="{{ url('/') }}/imagecache/emailsub/{{$smallStoryImages[2]->filename}}" /></div>
 
-                                            <div class="text-box" style="padding-top: 5px; padding-bottom: 10px; float: left; position: relative; box-sizing: border-box;">
+                                            <div class="text-box">
                                                 <h3 class="mid"><a href="{{ url('/') . '/story/' .$mainStories[2]->story_type . '/' . $mainStories[2]->id }}">{{ $mainStoryImages[2]->title }} &#10137;</a></h3>
                                                                                    {{--<p>{!! str_limit($mainStoryImages[2]->teaser, $limit = 110, $end = '...') !!}</p>--}}
                                                                                    {!! truncateLimitWords($mainStoryImages[2]->teaser, 110) !!}</div>
