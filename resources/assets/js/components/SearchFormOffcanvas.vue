70b73f4e21320784dd2889d8275b573316740472
Chris Puzzuoli <cpuzzuol@emich.edu>
2018-01-11 16:22:16 -0500

    fixed offcanvas search button empty text for sr

--- resources/assets/js/components/SearchFormOffcanvas.vue
+++ resources/assets/js/components/SearchFormOffcanvas.vue
@@ -6,7 +6,7 @@
             <label class="show-for-sr" for="offcanvassearch">Search EMU Today</label>
             <input class="input-group-field" id="offcanvassearch" type="text" name="searchterm" placeholder="Search" v-model="searchterm">
             <div class="input-group-button">
-            <button type="submit" class="button secondary small"><i class="fa fa-search"></i></span></button>
+            <button type="submit" class="button secondary small"><i class="fa fa-search"></i></span><span class="show-for-sr">submit search</span></button>
             </div>
         </div>
     </form>
