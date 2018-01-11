9a19f7913acb2bb5a4dec209e7e76a4addce37b2
Chris Puzzuoli <cpuzzuol@emich.edu>
2018-01-11 16:18:20 -0500

    fixed offcanvas search missing label

--- resources/assets/js/components/SearchFormOffcanvas.vue
+++ resources/assets/js/components/SearchFormOffcanvas.vue
@@ -3,7 +3,8 @@
         <slot name="csrf"></slot>
         <div class="input-group">
             <span class="show-for-sr">Search</span>
-            <input class="input-group-field" type="text" name="searchterm" placeholder="Search" v-model="searchterm">
+            <label class="show-for-sr" for="offcanvassearch">Search EMU Today</label>
+            <input class="input-group-field" id="offcanvassearch" type="text" name="searchterm" placeholder="Search" v-model="searchterm">
             <div class="input-group-button">
             <button type="submit" class="button secondary small"><i class="fa fa-search"></i></span></button>
             </div>
