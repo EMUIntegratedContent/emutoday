a428ea7f6956a48e8868437dc4ed59484e50b905
Chris Puzzuoli <cpuzzuol@emich.edu>
2018-05-04 12:43:09 -0400

    story idea email work

--- resources/views/admin/storyideas/email.blade.php
+++ resources/views/admin/storyideas/email.blade.php
@@ -22,7 +22,7 @@
       <li><strong>Deadline: </strong>{{ Carbon\Carbon::parse($idea->deadline)->format('m/d/y') }}</li>
       <li>
         @if($idea->assignee)
-          Assigned to {{ $idea->assignee }} {{ $idea->assignee }}
+          Assigned to {{ $idea->assignee->first_name }} {{ $idea->assignee }}
         @else
           Not assigned
         @endif
