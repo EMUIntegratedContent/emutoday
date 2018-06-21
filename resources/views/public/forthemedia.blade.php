dfb724413c261d9db12293facfb2336205e26cc8
Chris Puzzuoli <cpuzzuol@emich.edu>
2018-06-21 14:35:25 -0400

    added dean's list link to for the media public page

--- resources/views/public/forthemedia.blade.php
+++ resources/views/public/forthemedia.blade.php
@@ -1,45 +1,53 @@
 {{-- searchResults --}}
 @extends('public.layouts.global')
 @section('content')
-  <div id="content-area">
-    <div id="listing-bar">
-      <div class="row">
-        <div class="large-12 medium-12 small-12 column">
-          <div id="title-grouping" class="row">
-            <div class="large-6 medium-12 columns"><h3 class="news-caps">For the Media</h3></div>
-          </div>
-          <div class="row">
-              <div class="large-3 medium-3 small-12 columns">
-                  <ul class="side-nav experts-nav">
-                    <li><a class="home-style" href="/forthemedia">Resources</a></li>
-                    <li><a href="/experts">Find an Expert</a></li>
-                    <li><a href="/mediahighlights">Media Highlights</a></li>
-                  </ul>
-              </div>
-              <div class="large-9 medium-9 small-12 columns ">
-                <h1 style="padding-top:7px;">Media Resources</h1>
-                <p>The media relations team writes and distributes content about faculty research, student accomplishments and general EMU initiatives to local, state and national media outlets. We also help manage crisis communications for the university, respond to media inquiries, provide media training to faculty and administrators, and advise the colleges on communication strategies.</p>
-                <h2>Contact</h2>
-                <p>
-                  Geoff Larcom, Executive Director, Media Relations<br />
-                  734.487.4400<br />
-                  cell: 734.417.9658<br />
-                  glarcom@emich.edu
-                </p>
-                <h2>Details about Eastern Michigan University</h2>
-                <ul>
-                  <li><a href="https://www.emich.edu/president/">President James Smith</a></li>
-                  <li><a href="https://www.emich.edu/regents/">Board of Regents</a></li>
-                  <li><a href="https://www.emich.edu/police/about/safety-initiatives.php">Safety Initiatives</a></li>
-                  <li><a href="https://www.emich.edu/facts/index.php">University Facts</a></li>
-                  <li><a href="https://irim.emich.edu/">Enrollment Statistics</a></li>
-                  <li><a href="https://www.emich.edu/legalaffairs/foia.php">Freedom of Information Act (FOIA)</a></li>
-                  <li><a href="https://www.emich.edu/legalaffairs/foia.php">Extreme Weather Policy</a></li>
-                </ul>
-              </div>
-          </div>
+    <div id="content-area">
+        <div id="listing-bar">
+            <div class="row">
+                <div class="large-12 medium-12 small-12 column">
+                    <div id="title-grouping" class="row">
+                        <div class="large-6 medium-12 columns"><h3 class="news-caps">For the Media</h3></div>
+                    </div>
+                    <div class="row">
+                        <div class="large-3 medium-3 small-12 columns">
+                            <ul class="side-nav experts-nav">
+                                <li><a class="home-style" href="/forthemedia">Resources</a></li>
+                                <li><a href="/experts">Find an Expert</a></li>
+                                <li><a href="/mediahighlights">Media Highlights</a></li>
+                            </ul>
+                        </div>
+                        <div class="large-9 medium-9 small-12 columns ">
+                            <h1 style="padding-top:7px;">Media Resources</h1>
+                            <p>The media relations team writes and distributes content about faculty research, student
+                                accomplishments and general EMU initiatives to local, state and national media outlets.
+                                We also help manage crisis communications for the university, respond to media
+                                inquiries, provide media training to faculty and administrators, and advise the colleges
+                                on communication strategies.</p>
+                            <h2>Contact</h2>
+                            <p>
+                                Geoff Larcom, Executive Director, Media Relations<br/>
+                                734.487.4400<br/>
+                                cell: 734.417.9658<br/>
+                                glarcom@emich.edu
+                            </p>
+                            <h2>Details about Eastern Michigan University</h2>
+                            <ul>
+                                <li><a href="https://www.emich.edu/president/">President James Smith</a></li>
+                                <li><a href="https://www.emich.edu/regents/">Board of Regents</a></li>
+                                <li><a href="https://www.emich.edu/police/about/safety-initiatives.php">Safety
+                                        Initiatives</a></li>
+                                <li><a href="https://www.emich.edu/facts/index.php">University Facts</a></li>
+                                <li><a href="https://irim.emich.edu/">Enrollment Statistics</a></li>
+                                <li><a href="https://www.emich.edu/legalaffairs/foia.php">Freedom of Information Act
+                                        (FOIA)</a></li>
+                                <li><a href="https://www.emich.edu/legalaffairs/foia.php">Extreme Weather Policy</a>
+                                </li>
+                                <li><a href="https://www.emich.edu/registrar/forms/index.php">Dean's List</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>   <!--end content area-->
+    </div>   <!--end content area-->
 @endsection
