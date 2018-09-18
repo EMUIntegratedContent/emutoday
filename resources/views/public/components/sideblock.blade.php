1c504a92b3302d20241dc9274f8fe14d8912cfd5
Chris Puzzuoli <cpuzzuol@emich.edu>
2018-09-18 09:53:44 -0400

    fixed 'more stories' link to dynamic URL on individual stories page

--- resources/views/public/components/sideblock.blade.php
+++ resources/views/public/components/sideblock.blade.php
@@ -1,7 +1,7 @@
 @unless(count($sideitems) < 1)
 <div class="row" >
     <div class="small-12 medium-12 large-12 columns" >
-        <h3 class="subhead-title"><a class="bold-green-link" href="http://emutoday.test/story/news">More Stories</a></h3>
+        <h3 class="subhead-title"><a class="bold-green-link" href="{{ url('/story/news') }}">More Stories</a></h3>
         <div id="five-stories-bar">
             <div class="row large-up-5 medium-up-3 show-for-medium" data-equalizer>
               @each('public.featuredstoryhub', $sideitems, 'fstory')
