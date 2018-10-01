6371a2031b2b4c2a7c596a19297594bde8440d93
Chris Puzzuoli <cpuzzuol@emich.edu>
2018-10-01 11:15:39 -0400

    added pic of pres smith, initial styling

--- resources/views/public/todayemail/email.blade.php
+++ resources/views/public/todayemail/email.blade.php
@@ -357,12 +357,10 @@
                        <td valign="top" >
                            <div class="indent">
                                <table border="0" style="max-width: 100%;">
-                                   <tr height="136px">
+                                   <tr height="136px" class="moveover" style="border-top: 3px double #97D700;">
                                        <td>
                                            <img src="{{ url('/') }}/assets/imgs/email/president-jim-smith-136px.png" alt="EMU President Jim Smith" width="109px" /></a>
                                        </td>
-                                   </tr>
-                                   <tr valign="top">
                                        <td>
                                            <h2><a href="{{ $email->president_url }}">President Smith Statement &#10137;</a></h2>
                                            <p style="padding:5px 0 13px 8px">{{ $email->president_teaser }}</p>
