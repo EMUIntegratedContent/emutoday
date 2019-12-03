797dea1b8c04dcfe79f0e2691aa5e5e52de3cd6e
Brian Koscielniak <bkosciel@emich.edu>
2019-12-03 13:03:48 -0500

    display adjustments on magazine

--- resources/views/public/magazine/index.blade.php
+++ resources/views/public/magazine/index.blade.php
@@ -1,90 +1,85 @@
 {{-- Magazine Index Page --}}
 @extends('public.layouts.global')
 @section('offcanvaslist')
-    @include('public.magazine.partials.offcanvaslist')
+@include('public.magazine.partials.offcanvaslist')
 @endsection
-  @section('connectionbar')
-    @include('public.magazine.partials.connectionbar')
-  @endsection
+@section('connectionbar')
+@include('public.magazine.partials.connectionbar')
+@endsection
 
 @section('magazine-title', 'Eastern Magazine')
 @section('content')
-  <div id="content-area">
+<div id="content-area">
     <div id="homepage-hero" class="row column">
-      <img src="/imagecache/original/{{$heroImg->filename}}" alt="main image">
-      <div id="magazine-text-over-image-box" class="row collapse">
-        <div class="centered-main-title">
-          <h2><a href="/magazine/article/{{$heroImg->story->id}}">{{$heroImg->title}}</a></h2>
-          <p>{{$heroImg->caption}}</p>
+        <img src="/imagecache/original/{{$heroImg->filename}}" alt="main image">
+        <div id="magazine-text-over-image-box" class="row collapse">
+            <div class="centered-main-title">
+                <h2><a href="/magazine/article/{{$heroImg->story->id}}">{{$heroImg->title}}</a></h2>
+                <p>{{$heroImg->caption}}</p>
+            </div>
         </div>
-      </div>
     </div>
     <div id="profiles-list" >
-      <div class="row column">
-        <div class="row small-up-2 medium-up-2 large-up-5">
-          @foreach ($barImgs as $barImg)
-            <div class="column">
-              <a class="article-link"
-              @if($barImg->story->tags()->first())
-                  @if($barImg->story->tags()->first()->name == 'external')
+        <div class="row column">
+            <div class="row small-up-2 medium-up-2 large-up-5">
+                @foreach ($barImgs as $barImg)
+                <div class="column">
+                    <a class="article-link"
+                    @if($barImg->story->tags()->first())
+                    @if($barImg->story->tags()->first()->name == 'external')
                     href="{{$barImg->link}}"
-                  @else
+                    @else
                     href="/magazine/article/{{$barImg->story->id}}"
-                  @endif
-              @else
-                href="/magazine/article/{{$barImg->story->id}}"
-              @endif
-              >
-                <img class="topic-image" src="/imagecache/original/{{$barImg->filename}}"  alt="topic image"/>
-                <div class="profile-content">
-                  <div class="profile-text-content magazine" data-equalizer-watch>
-                    <h3>{{$barImg->title}}</h3>
-                    <p>{{$barImg->caption}}</p>
-                  </div>
-                </div>
-              </a>
+                    @endif
+                    @else
+                    href="/magazine/article/{{$barImg->story->id}}"
+                    @endif
+                    >
+                    <img class="topic-image" src="/imagecache/original/{{$barImg->filename}}"  alt="topic image"/>
+                    <div class="profile-content">
+                        <div class="profile-text-content magazine" data-equalizer-watch>
+                            <h3>{{$barImg->title}}</h3>
+                            <p>{{$barImg->caption}}</p>
+                        </div>
+                    </div>
+                </a>
             </div>
-          @endforeach
+            @endforeach
         </div>
-      </div>
-      <div id="magazine-feature">
-        <div class="row">
-          <div class="large-7 large-push-5 medium-12 small-12 columns">
-            <div class="row collapse photo-feature-section">
-              <div class="large-6 medium-4 small-12 columns"><img class="topic-image contributor" src="/imagecache/original/{{$magazineExtra->media_name}}"  alt="back page image"/>
-              </div>
-              <div class="large-6 medium-8 small-12 columns photo-feature-text ">
-                <h5>{{$magazineExtra->headline}}</h5>
-                {!! $magazineExtra->teaser !!}
-                <p class="author">{{$magazineExtra->caption}}</p>
-                @if($magazineExtra->link != '')
-                  <p class="button-container"><a class="button expanded magazine-button" href="{{$magazineExtra->link}}">{{$magazineExtra->link_text}}</a></p>
-                @endif
-              </div>
-
-            </div>
-
-          </div>
-
-
-          <div class="large-5 large-pull-7 medium-12 small-12 columns">
-            <div class="row collapse">
-              <div class="large-6 medium-3 show-for-medium columns cover-box">
-                                <img class="topic-image magazine-cover" src="/imagecache/magazinecover/{{$magazineCover->media_name}}"  alt="magazine image"/>
-              </div>
-              <div class="large-6 medium-9 small-12 columns magazine-details">
-                                <h4>{{$magazineCover->headline}}</h4>
-                                {!! $magazineCover->teaser !!}
-                                <p><a href="{{$magazineCover->link}}">{{$magazineCover->link_text}}</a></p>
-                <p class="button-container"><a class="button expanded magazine-button" href="mailto:dgiffor2@emich.edu">Subscribe</a></p>
-                <p class="button-container"><a class="button expanded magazine-button" href="mailto:dgiffor2@emich.edu">Submit a story idea</a></p>
-              </div>
-
-            </div>
-          </div>
-
-        </div>
-      </div><!--end of profiles-list-->
-
     </div>
+
+    <div id="magazine-feature">
+        <div class="row">
+            <div class="large-7 large-push-5 medium-12 small-12 columns">
+                <div class="row photo-feature-section">
+                    <div class="large-6 medium-4 small-12 columns"><img class="topic-image contributor" src="/imagecache/original/{{$magazineExtra->media_name}}"  alt="back page image"/>
+                    </div>
+                    <div class="large-6 medium-8 small-12 columns photo-feature-text ">
+                        <h5>{{$magazineExtra->headline}}</h5>
+                        {!! $magazineExtra->teaser !!}
+                        <p class="author">{{$magazineExtra->caption}}</p>
+                        @if($magazineExtra->link != '')
+                        <p class="button-container"><a class="button expanded magazine-button" href="{{$magazineExtra->link}}">{{$magazineExtra->link_text}}</a></p>
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+            <div class="large-5 large-pull-7 medium-12 small-12 columns">
+                <div class="row">
+                    <div class="large-6 medium-3 show-for-medium columns cover-box">
+                        <img class="topic-image magazine-cover" src="/imagecache/magazinecover/{{$magazineCover->media_name}}"  alt="magazine image"/>
+                    </div>
+                    <div class="large-6 medium-9 small-12 columns magazine-details">
+                        <h4>{{$magazineCover->headline}}</h4>
+                        {!! $magazineCover->teaser !!}
+                        <p><a href="{{$magazineCover->link}}">{{$magazineCover->link_text}}</a></p>
+                        <p class="button-container"><a class="button expanded magazine-button" href="mailto:dgiffor2@emich.edu">Subscribe</a></p>
+                        <p class="button-container"><a class="button expanded magazine-button" href="mailto:dgiffor2@emich.edu">Submit a story idea</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!--end of profiles-list-->
+</div>
 @endsection
--- resources/views/public/magazine/issue.blade.php
+++ resources/views/public/magazine/issue.blade.php
@@ -21,30 +21,30 @@
                   <div class="addthis magazine-top"><a href="#"><img src="/assets/imgs/icons/fake-addthis.png" alt="addthis "></a></div>
                 </div>
               </div> <!-- issue-grouping -->
-              <div id="story-listing" class="row">
-                                  @foreach ($barImgs as $barImg)
-                                        <div class="article-listing row column">
-                                        <a class="magazine-article-button"
-                                        @if($barImg->story->tags()->first())
-                                            @if($barImg->story->tags()->first()->name == 'external')
-                                              href="{{$barImg->link}}"
-                                            @else
-                                              href="/magazine/article/{{$barImg->story->id}}"
-                                            @endif
-                                        @else
-                                          href="/magazine/article/{{$barImg->story->id}}"
-                                        @endif
-                                        >
-                                        <div class="article-img small-12 medium-5 large-3 columns">
-                                                <img src="/imagecache/original/{{$barImg->filename}}">
-                                        </div>
-                                        <div class="article-text-teaser small-12 medium-7 large-9 columns">
-                                            <h3>{{$barImg->title}}</h3>
-                                            <p>{{$barImg->teaser}}</p>
-                                        </div>
-                                    </a>
-                                    </div>
-                                    @endforeach
+              <div id="story-listing">
+                  @foreach ($barImgs as $barImg)
+                        <a class="magazine-article-button"
+                                @if($barImg->story->tags()->first())
+                                    @if($barImg->story->tags()->first()->name == 'external')
+                                      href="{{$barImg->link}}"
+                                    @else
+                                      href="/magazine/article/{{$barImg->story->id}}"
+                                    @endif
+                                @else
+                                  href="/magazine/article/{{$barImg->story->id}}"
+                                @endif
+                                >
+                        <div class="article-listing row">
+                            <div class="article-img small-12 medium-5 large-3 columns">
+                                <img src="/imagecache/original/{{$barImg->filename}}">
+                            </div>
+                            <div class="article-text-teaser small-12 medium-7 large-9 columns">
+                                <h3>{{$barImg->title}}</h3>
+                                <p>{{$barImg->teaser}}</p>
+                            </div>
+                        </div>
+                    </a>
+                @endforeach
               </div> <!-- story-listing -->
             </div>
             <div class="large-3 medium-4 small-12 columns current-issue-padding">
