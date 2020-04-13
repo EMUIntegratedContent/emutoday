6e73e0011e12ed526fc3c83d9834c54341262e8f
Brian Koscielniak <bkosciel@emich.edu>
2020-04-13 17:06:27 -0400

    jQuery wasn't working on frontend so I reverted public-script.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,7 +20,7 @@ const mix = require('laravel-mix');
 |
 */
 // Compile the front-end SASS styles
-//mix.sass('resources/assets/sass/public-app.scss', '../resources/assets/css/zfoundation.css');
+// mix.sass('resources/assets/sass/public-app.scss', '../resources/assets/css/zfoundation.css');
 
 // Combine all the front-end styles into one stylesheet
 mix.combine([
@@ -42,37 +42,37 @@ mix.combine([
 | The scripts that will run on the public facing pages
 |
 */
-mix.combine([
-    'resources/assets/js/vendor-public/jquery.min.js',
-    'node_modules/foundation-sites/js/foundation.core.js',
-    'node_modules/foundation-sites/js/foundation.abide.js',
-    'node_modules/foundation-sites/js/foundation.accordion.js',
-    'node_modules/foundation-sites/js/foundation.accordionMenu.js',
-    'node_modules/foundation-sites/js/foundation.drilldown.js',
-    'node_modules/foundation-sites/js/foundation.dropdown.js',
-    'node_modules/foundation-sites/js/foundation.dropdownMenu.js',
-    'node_modules/foundation-sites/js/foundation.equalizer.js',
-    // 'node_modules/foundation-sites/js/foundation.interchange.js',
-    // 'node_modules/foundation-sites/js/foundation.magellan.js',
-    'node_modules/foundation-sites/js/foundation.offcanvas.js',
-    // 'node_modules/foundation-sites/js/foundation.orbit.js',
-    'node_modules/foundation-sites/js/foundation.responsiveMenu.js',
-    'node_modules/foundation-sites/js/foundation.responsiveToggle.js',
-    'node_modules/foundation-sites/js/foundation.reveal.js',
-    'node_modules/foundation-sites/js/foundation.slider.js',
-    'node_modules/foundation-sites/js/foundation.sticky.js',
-    // 'node_modules/foundation-sites/js/foundation.tabs.js',
-    // 'node_modules/foundation-sites/js/foundation.toggler.js',
-    'node_modules/foundation-sites/js/foundation.tooltip.js',
-    'node_modules/foundation-sites/js/foundation.util.box.js',
-    'node_modules/foundation-sites/js/foundation.util.keyboard.js',
-    'node_modules/foundation-sites/js/foundation.util.mediaQuery.js',
-    'node_modules/foundation-sites/js/foundation.util.motion.js',
-    'node_modules/foundation-sites/js/foundation.util.nest.js',
-    'node_modules/foundation-sites/js/foundation.util.timerAndImageLoader.js',
-    'node_modules/foundation-sites/js/foundation.util.touch.js',
-    'node_modules/foundation-sites/js/foundation.util.triggers.js',
-], 'public/js/public-scripts.js');
+// mix.combine([
+//     'resources/assets/js/vendor-public/jquery.min.js',
+//     'node_modules/foundation-sites/js/foundation.core.js',
+//     'node_modules/foundation-sites/js/foundation.abide.js',
+//     'node_modules/foundation-sites/js/foundation.accordion.js',
+//     'node_modules/foundation-sites/js/foundation.accordionMenu.js',
+//     'node_modules/foundation-sites/js/foundation.drilldown.js',
+//     'node_modules/foundation-sites/js/foundation.dropdown.js',
+//     'node_modules/foundation-sites/js/foundation.dropdownMenu.js',
+//     'node_modules/foundation-sites/js/foundation.equalizer.js',
+//     // 'node_modules/foundation-sites/js/foundation.interchange.js',
+//     // 'node_modules/foundation-sites/js/foundation.magellan.js',
+//     'node_modules/foundation-sites/js/foundation.offcanvas.js',
+//     // 'node_modules/foundation-sites/js/foundation.orbit.js',
+//     'node_modules/foundation-sites/js/foundation.responsiveMenu.js',
+//     'node_modules/foundation-sites/js/foundation.responsiveToggle.js',
+//     'node_modules/foundation-sites/js/foundation.reveal.js',
+//     'node_modules/foundation-sites/js/foundation.slider.js',
+//     'node_modules/foundation-sites/js/foundation.sticky.js',
+//     // 'node_modules/foundation-sites/js/foundation.tabs.js',
+//     // 'node_modules/foundation-sites/js/foundation.toggler.js',
+//     'node_modules/foundation-sites/js/foundation.tooltip.js',
+//     'node_modules/foundation-sites/js/foundation.util.box.js',
+//     'node_modules/foundation-sites/js/foundation.util.keyboard.js',
+//     'node_modules/foundation-sites/js/foundation.util.mediaQuery.js',
+//     'node_modules/foundation-sites/js/foundation.util.motion.js',
+//     'node_modules/foundation-sites/js/foundation.util.nest.js',
+//     'node_modules/foundation-sites/js/foundation.util.timerAndImageLoader.js',
+//     'node_modules/foundation-sites/js/foundation.util.touch.js',
+//     'node_modules/foundation-sites/js/foundation.util.triggers.js',
+// ], 'public/js/public-scripts.js');
 
 // Compile vue files
 mix.js('resources/assets/js/vue-caleventview.js', 'public/js/vue-caleventview.js');
