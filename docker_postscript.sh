382d1860e6990bbde7330a9b04e2a76aaaf6a68f
Chris Puzzuoli <cpuzzuol@gmail.com>
2023-04-26 22:06:50 -0400

    update dockerfiles

--- docker_postscript.sh
+++ docker_postscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Change ownership
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/public
 
 # Change permissions
-cmmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+cmmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/public
 
 # Start Apache in the foreground. This takes the place of "CMD ["apache2-foreground"]" in the Dockerfile!
 exec apache2-foreground
