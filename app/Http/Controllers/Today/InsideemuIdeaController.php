9bfee7dabf0e045a133c263e34de507ba0dd4ed6
Chris Puzzuoli <cpuzzuol@umich.edu>
2024-10-09 15:06:42 -0400

    allow non-admins to access public ideas submission

--- app/Http/Controllers/Today/InsideemuIdeaController.php
+++ app/Http/Controllers/Today/InsideemuIdeaController.php
@@ -11,7 +11,7 @@ class InsideemuIdeaController extends Controller
 
 		public function __construct(InsideemuIdea $idea){
 			$this->idea = $idea;
-			$this->middleware('auth', []);
+			$this->middleware('auth', ['only' => ['create', 'edit']]);
 		}
 
     /**
@@ -19,10 +19,7 @@ class InsideemuIdeaController extends Controller
      */
     public function index()
     {
-			$user = null;
-			if (\Auth::check()) {
-				$user = cas()->user();
-			}
+			$user = cas()->user();
 			return view('public.insideemu.ideas.index', compact('user'));
     }
 
@@ -31,57 +28,18 @@ class InsideemuIdeaController extends Controller
      */
     public function create()
     {
-			$user = null;
-			if (\Auth::check()) {
-				$user = cas()->user();
-			}
-
+			$user = cas()->user();
 			return view('public.insideemu.ideas.new', compact('user'));
     }
-//
-//    /**
-//     * Store a newly created resource in storage.
-//     */
-//    public function store(StoreInsideemuPostRequest $request)
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Display the specified resource.
-//     */
-//    public function show(InsideemuPost $insideemuPost)
-//    {
-//        //
-//    }
-//
+
     /**
      * Show the form for editing the specified resource.
      */
     public function edit(InsideemuIdea $idea)
     {
 			$ideaId  = $idea->id;
-			$user = null;
-			if (\Auth::check()) {
-				$user = cas()->user();
-			}
+			$user = cas()->user();
 
 			return view('public.insideemu.ideas.edit', compact('ideaId', 'user'));
     }
-//
-//    /**
-//     * Update the specified resource in storage.
-//     */
-//    public function update(UpdateInsideemuPostRequest $request, InsideemuPost $insideemuPost)
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Remove the specified resource from storage.
-//     */
-//    public function destroy(InsideemuPost $insideemuPost)
-//    {
-//        //
-//    }
 }
