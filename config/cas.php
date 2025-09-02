51885cd55ce48e98afe0233da10b9ece31586dbe
Chris Puzzuoli <cpuzzuol@umich.edu>
2025-09-02 10:52:46 -0400

    another cas

--- config/cas.php
+++ config/cas.php
@@ -65,7 +65,7 @@ return [
 	|--------------------------------------------------------------------------
 	| Example: 'http://localhost', 'https://example.com:8888'
 	*/
-	'cas_client_service' => env('CAS_CLIENT_SERVICE', request()->getSchemeAndHttpHost() . '/cas/callback'),
+	'cas_client_service' => env('CAS_CLIENT_SERVICE', 'http://localhost:8080'),
 
 
 	/*
