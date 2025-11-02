7ee5d48946ebca1a12f8e6dc1609856e4a97d93f
Chris Puzzuoli <cpuzzuol@umich.edu>
2025-11-02 09:21:54 -0500

    resize inside emu image submissions to 1280px max if larger

--- app/Services/InsideemuService.php
+++ app/Services/InsideemuService.php
@@ -26,21 +26,21 @@ class InsideemuService
 	public function handleIdeaImages(InsideemuIdea $idea, array $ideaArr)
 	{
 		$images = $idea->images; // the insideemu_ideas_images records already associated with the idea
-		foreach($images as $image){
+		foreach ($images as $image) {
 			$found = false;
 			// The images that were passed in the request
-			foreach($ideaArr['images'] as $newImage){
-				if($image->id == $newImage['id']){ // If there is a match, the image is still associated with the idea
+			foreach ($ideaArr['images'] as $newImage) {
+				if ($image->id == $newImage['id']) { // If there is a match, the image is still associated with the idea
 					$found = true;
 					break;
 				}
 			}
 			// Remove the insideemu_ideas_images record and the image file if it is no longer associated with the idea
-			if(!$found){
+			if (!$found) {
 				$image->delete();
 				// Also remove the file from the server
 				$filePath = public_path() . $image->image_path;
-				if(file_exists($filePath)){
+				if (file_exists($filePath)) {
 					unlink($filePath);
 				}
 			}
@@ -48,9 +48,9 @@ class InsideemuService
 
 		// Handle any new images files from the request.
 		$newImgFileNames = []; // store new, timestamped and sanitized image names
-		$destinationFolder = '/imgs/uploads/insideemu/ideas/'.$idea->id.'/';
-		if (!empty(Input::file('images'))){
-			foreach(Input::file('images') as $image){
+		$destinationFolder = '/imgs/uploads/insideemu/ideas/' . $idea->id . '/';
+		if (!empty(Input::file('images'))) {
+			foreach (Input::file('images') as $image) {
 				// If the path doesn't exist, create it
 				if (!file_exists(public_path() . $destinationFolder)) {
 					mkdir(public_path() . $destinationFolder, 0777, true);
@@ -62,23 +62,31 @@ class InsideemuService
 
 				$newImgFileNames[$image->getClientOriginalName()] = $imgFileName;
 
+				// Resize image to max width/height to prevent memory exhaustion (only resize if the image is larger than the max width/height)
+				$maxWidth = 1280;
+				$maxHeight = 1280;
+
 				Image::read($imgFilePath)
+					->scaleDown(width: $maxWidth, height: $maxHeight)
 					->save(public_path() . $destinationFolder . $imgFileName);
+
+				// Image::read($imgFilePath)
+				// 	->save(public_path() . $destinationFolder . $imgName);
 			}
 		}
 
 		// Update the database with new and existing image information
-		foreach($ideaArr['images'] as $image){
+		foreach ($ideaArr['images'] as $image) {
 			// If the id contains 'new', it's a new image
-			if(strpos($image['id'], 'new') !== false){
+			if (strpos($image['id'], 'new') !== false) {
 				$ideaImage = new InsideemuIdeasImages();
 				$ideaImage->insideemu_idea_id = $idea->id;
 
 				// Find the new image name in the array of new, sanitized image names (avoids different timestamp issues)
-				foreach($newImgFileNames as $originalName => $newName){
-					if($image['image_name'] == $originalName){
+				foreach ($newImgFileNames as $originalName => $newName) {
+					if ($image['image_name'] == $originalName) {
 						$ideaImage->image_name = $newName;
-						$ideaImage->image_path = $destinationFolder.$newName;
+						$ideaImage->image_path = $destinationFolder . $newName;
 						break;
 					}
 				}
@@ -92,21 +100,21 @@ class InsideemuService
 
 	public function handlePostImages(InsideemuPost $post, array $postArr)
 	{
-		$destinationFolder = '/imgs/insideemu_posts/'.$post->id.'/';
+		$destinationFolder = '/imgs/insideemu_posts/' . $post->id . '/';
 
 		$images = $post->images;
 
 		$changedFileNames = []; // store existing images whose updates resulted in new, timestamped and sanitized image names
-		foreach($images as $image){
+		foreach ($images as $image) {
 			$found = false;
 			// Check if each $postArr['images'] id matches an image id
 			// $postArr['images'] is the array of images from the request (corresponding to insideemu_post_images records)
-			foreach($postArr['images'] as $newImage){
-				if($image->id == $newImage['id']){
+			foreach ($postArr['images'] as $newImage) {
+				if ($image->id == $newImage['id']) {
 					// If the image's name or path has changed, update it in the filesystem
 					$oldFilePath = public_path() . $image->image_path;
 					$newFilePath = public_path() . $destinationFolder . $newImage['image_name'];
-					if(file_exists($oldFilePath) && $oldFilePath != $newFilePath){
+					if (file_exists($oldFilePath) && $oldFilePath != $newFilePath) {
 						$sanitizedNewFile = $this->sanitizeImageName($newImage['image_name']);
 						$newFilename = $this->appendNewTimestamp($sanitizedNewFile);
 						$sanitizedNewPath = public_path() . $destinationFolder . $newFilename;
@@ -119,11 +127,11 @@ class InsideemuService
 				}
 			}
 			// Remove any images that are no longer associated with the post
-			if(!$found){
+			if (!$found) {
 				$image->delete();
 				// Also remove the file from the server
 				$filePath = public_path() . $image->image_path;
-				if(file_exists($filePath)){
+				if (file_exists($filePath)) {
 					unlink($filePath);
 				}
 			}
@@ -131,10 +139,10 @@ class InsideemuService
 
 		// Handle any attached images files from the request (these will be new images)
 		$newImgFileNames = []; // store new, timestamped and sanitized image names
-		if (!empty(Input::file('images'))){
+		if (!empty(Input::file('images'))) {
 			$count = 0;
 			$imgNames = Input::get('imageNames');
-			foreach(Input::file('images') as $image){
+			foreach (Input::file('images') as $image) {
 				// If the path doesn't exist, create it
 				if (!file_exists(public_path() . $destinationFolder)) {
 					mkdir(public_path() . $destinationFolder, 0777, true);
@@ -143,8 +151,17 @@ class InsideemuService
 				$imgFilePath = $image->getRealPath();
 				$imgName = $this->sanitizeImageName($imgNames[$count]);
 				$imgName = $this->appendNewTimestamp($imgName);
+
+				// Resize image to max width/height to prevent memory exhaustion (only resize if the image is larger than the max width/height)
+				$maxWidth = 1280;
+				$maxHeight = 1280;
+
 				Image::read($imgFilePath)
+					->scaleDown(width: $maxWidth, height: $maxHeight)
 					->save(public_path() . $destinationFolder . $imgName);
+
+				// Image::read($imgFilePath)
+				// 	->save(public_path() . $destinationFolder . $imgName);
 
 				$newImgFileNames[$image->getClientOriginalName()] = $imgName;
 
@@ -153,18 +170,18 @@ class InsideemuService
 		}
 
 		// Update the database with new and existing image information
-		foreach($postArr['images'] as $image){
+		foreach ($postArr['images'] as $image) {
 			// If there is no id or if it's 0, it's a new image
-			if(!$image['id'] || $image['id'] == 0){
+			if (!$image['id'] || $image['id'] == 0) {
 				$postImage = new InsideemuPostsImages();
 				$postImage->insideemu_post_id = $post->id;
 				$postImage->imagetype_id = $image['imagetype_id'];
 
 				// Find the new image name in the array of new, sanitized image names (avoids different timestamp issues)
-				foreach($newImgFileNames as $originalName => $newName){
-					if($image['image_name'] == $originalName){
+				foreach ($newImgFileNames as $originalName => $newName) {
+					if ($image['image_name'] == $originalName) {
 						$postImage->image_name = $newName;
-						$postImage->image_path = $destinationFolder.$newName;
+						$postImage->image_path = $destinationFolder . $newName;
 						break;
 					}
 				}
@@ -174,12 +191,12 @@ class InsideemuService
 				// If the image's name has changed, update it in the filesystem
 				$oldName = $postImage->image_name;
 				$newPath = $image['image_name'];
-				if($oldName != $newPath){
+				if ($oldName != $newPath) {
 					// Find the new image name in the array of new, sanitized image names (avoids different timestamp issues)
-					foreach($changedFileNames as $originalName => $newName){
-						if($image['image_name'] == $originalName){
+					foreach ($changedFileNames as $originalName => $newName) {
+						if ($image['image_name'] == $originalName) {
 							$postImage->image_name = $newName;
-							$postImage->image_path = $destinationFolder.$newName;
+							$postImage->image_path = $destinationFolder . $newName;
 							break;
 						}
 					}
@@ -203,11 +220,11 @@ class InsideemuService
 	 */
 	public function deletePostImages(string $postId)
 	{
-		$destinationFolder = '/imgs/insideemu_posts/'.$postId.'/';
-		if(file_exists(public_path() . $destinationFolder)){
-			$files = glob(public_path() . $destinationFolder.'*');
-			foreach($files as $file){
-				if(is_file($file)){
+		$destinationFolder = '/imgs/insideemu_posts/' . $postId . '/';
+		if (file_exists(public_path() . $destinationFolder)) {
+			$files = glob(public_path() . $destinationFolder . '*');
+			foreach ($files as $file) {
+				if (is_file($file)) {
 					unlink($file);
 				}
 			}
@@ -221,11 +238,11 @@ class InsideemuService
 	 */
 	public function deleteIdeaImages(string $ideaId)
 	{
-		$destinationFolder = '/imgs/uploads/insideemu/ideas/'.$ideaId.'/';
-		if(file_exists(public_path() . $destinationFolder)){
-			$files = glob(public_path() . $destinationFolder.'*');
-			foreach($files as $file){
-				if(is_file($file)){
+		$destinationFolder = '/imgs/uploads/insideemu/ideas/' . $ideaId . '/';
+		if (file_exists(public_path() . $destinationFolder)) {
+			$files = glob(public_path() . $destinationFolder . '*');
+			foreach ($files as $file) {
+				if (is_file($file)) {
 					unlink($file);
 				}
 			}
@@ -238,14 +255,15 @@ class InsideemuService
 	 * @param $text
 	 * @return string
 	 */
-	public function replaceNewlinesWithPTags($text) {
+	public function replaceNewlinesWithPTags($text)
+	{
 		// Split the text by newlines
 		$lines = explode("\n", $text);
 
 		// Wrap each line with <p> tags
-		$wrappedLines = array_map(function($line) {
+		$wrappedLines = array_map(function ($line) {
 			$tline = trim($line);
-			if(empty($tline)) return ''; // Skip empty lines
+			if (empty($tline)) return ''; // Skip empty lines
 			return '<p>' . $tline . '</p>';
 		}, $lines);
 
@@ -259,7 +277,8 @@ class InsideemuService
 	 * @param string $imgName
 	 * @return string
 	 */
-	protected function sanitizeImageName ($imgName) {
+	protected function sanitizeImageName($imgName)
+	{
 		// Extract the base name and remove the timestamp portion
 		$filenameWithoutExtension = pathinfo($imgName, PATHINFO_FILENAME);
 
